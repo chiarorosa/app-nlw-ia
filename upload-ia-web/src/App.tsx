349933418098b1c1d2d58ad6a2deeb42e1ed575d
chiarorosa-m1 <pablo.chiaro.rosa@gmail.com>
2023-09-12 13:56:55 -0300

    inseridas textareas frontend react

--- upload-ia-web/src/App.tsx
+++ upload-ia-web/src/App.tsx
@@ -1,6 +1,7 @@
 import { Github } from "lucide-react"
 import { Button } from "./components/ui/button"
 import { Separator } from "./components/ui/separator"
+import { Textarea } from "./components/ui/textarea"
 
 export function App() {
 	return (
@@ -9,9 +10,7 @@ export function App() {
 				<h1 className="text-xl font-bold">upload.ai</h1>
 
 				<div className="flex items-center gap-3">
-					<span className="text-sm text-muted-foreground">
-						Desenvolvido no NLW-ia 🤖 2023
-					</span>
+					<span className="text-sm text-muted-foreground">Desenvolvido no NLW-ia 🤖 2023</span>
 
 					<Separator orientation="vertical" className="h-6" />
 
@@ -26,12 +25,12 @@ export function App() {
 			<main className="flex-1 p-6 flex gap-6">
 				{/* flex-col = coluna (vertical) e flex-1 é espaço disponível subtraindo o aside*/}
 				<div className="flex flex-col flex-1 gap-4">
-					<div className="grid grid-rows-2 gap-4 flex-1"></div>
-
+					<div className="grid grid-rows-2 gap-4 flex-1">
+						<Textarea className="resize-none p-4 leading-relaxed" placeholder="Insira o prompt para a IA..." />
+						<Textarea className="resize-none p-4 leading-relaxed" placeholder="Resultado gerado pela IA..." readOnly />
+					</div>
 					<p className="text-sm text-muted-foreground">
-						Lembre-se: você pode utilizar a variável{" "}
-						<code className="text-blue-400">{"{transcription}"}</code> no seu
-						prompt para adicionar o contéudo da transcrição do vídeo carregado.
+						Lembre-se: você pode utilizar a variável <code className="text-blue-400">{"{transcription}"}</code> no seu prompt para adicionaro contéudo da transcrição do vídeo carregado.
 					</p>
 				</div>
 				<aside className="w-80"></aside>
