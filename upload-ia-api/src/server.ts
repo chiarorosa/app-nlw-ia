82262362391bc045b8b7c421b1bdefcdfd59707b
chiarorosa-m1 <pablo.chiaro.rosa@gmail.com>
2023-09-13 17:03:37 -0300

    rota prompt criada

--- upload-ia-api/src/server.ts
+++ upload-ia-api/src/server.ts
@@ -1,9 +1,14 @@
+import { assert } from "console"
 import { fastify } from "fastify"
+import { prisma } from "./lib/prisma"
 
 const app = fastify()
 
-app.get("/", () => {
-	return { hello: "world" }
+app.get("/prompts", async () => {
+	// Utilizando o Prisma para buscar todos os prompts
+	const prompts = await prisma.prompt.findMany()
+
+	return prompts
 })
 
 app
