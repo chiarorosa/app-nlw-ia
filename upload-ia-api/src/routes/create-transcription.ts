aa30835c57f168ba5a188f731c7ada56c3c75177
chiarorosa-m1 <pablo.chiaro.rosa@gmail.com>
2023-09-15 12:39:15 -0300

    salvando no banco a transcricao do video

--- upload-ia-api/src/routes/create-transcription.ts
+++ upload-ia-api/src/routes/create-transcription.ts
@@ -37,6 +37,17 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
 			prompt,
 		})
 
-		return response.text
+		const transcription = response.text
+
+		await prisma.video.update({
+			where: {
+				id: videoId,
+			},
+			data: {
+				transcription: transcription,
+			},
+		})
+
+		return { transcription }
 	})
 }
