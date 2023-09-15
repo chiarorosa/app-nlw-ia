ab60478ffde0d1f721a4f235b85cbdda7d9304d7
chiarorosa-m1 <pablo.chiaro.rosa@gmail.com>
2023-09-15 13:03:00 -0300

    instalado fastify/cors para segurança de consumo da api em desenvolvimento

--- upload-ia-api/src/server.ts
+++ upload-ia-api/src/server.ts
@@ -1,11 +1,17 @@
 import { assert } from "console"
 import { fastify } from "fastify"
+import { fastifyCors } from "@fastify/cors"
 import { getAllPromptsRoute } from "./routes/get-all-prompts"
 import { uploadVideoRoute } from "./routes/upload-video"
 import { createTranscriptionRoute } from "./routes/create-transcription"
 import { generateAICompletionRoute } from "./routes/generate-ai-completion"
 
 const app = fastify()
+
+app.register(fastifyCors, {
+	origin: "*", // TODO: trocar para o domínio do site
+	methods: ["GET", "POST", "PUT", "DELETE"], // TODO: trocar para os métodos que serão usados
+})
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
