da9793955a1997d8a49121791aa152c40b3f49d8
chiarorosa-m1 <pablo.chiaro.rosa@gmail.com>
2023-09-15 13:00:07 -0300

    completion openai implementada

--- upload-ia-api/src/routes/generate-ai-completion.ts
+++ upload-ia-api/src/routes/generate-ai-completion.ts
@@ -6,7 +6,7 @@ import { openai } from "../lib/openai"
 
 // O fastify exige todos módulos de rotas sejam assíncronos
 export async function generateAICompletionRoute(app: FastifyInstance) {
-	app.post("/ai/complete", async (request) => {
+	app.post("/ai/complete", async (request, reply) => {
 		const bodySchema = z.object({
 			videoId: z.string().uuid(),
 			template: z.string(),
@@ -15,10 +15,26 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
 
 		const { videoId, template, temperature } = bodySchema.parse(request.body)
 
-		return {
-			videoId,
-			template,
-			temperature,
+		const video = await prisma.video.findUniqueOrThrow({
+			where: {
+				id: videoId,
+			},
+		})
+
+		if (!video.transcription) {
+			return reply.status(400).send({
+				error: "A transcrição não foi gerada ainda",
+			})
 		}
+
+		const promptMessage = template.replace("{transcription}", video.transcription)
+
+		const response = await openai.chat.completions.create({
+			model: "gpt-3.5-turbo",
+			temperature: temperature,
+			messages: [{ role: "user", content: promptMessage }],
+		})
+
+		return response
 	})
 }
